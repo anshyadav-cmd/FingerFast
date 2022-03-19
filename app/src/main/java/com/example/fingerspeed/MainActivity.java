dd77da23efed7c5233b8493185d7b035d8e3acaa
anshyadav <anshyadav4244@gmail.com>
2022-03-19 23:27:42 +0530

    Finised Project 2

--- app/src/main/java/com/example/fingerspeed/MainActivity.java
+++ app/src/main/java/com/example/fingerspeed/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fingerspeed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +8,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,11 +23,24 @@ public class MainActivity extends AppCompatActivity {
     private Button tapButton;
     private CountDownTimer countDownTimer;
 
+    private final String REMANING_TIME_KEY = "remaining time key";
+    private final String REMAING_TAP_KEY = "remaining taps key";
+
     private boolean isFirst = true;
     private long initialCundDownInMillis = 10_000l;
     private int timeIntervals = 1_000;
     private int remainingTime = (int)initialCundDownInMillis/1000;
     private int tapRemaming = 10;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(REMANING_TIME_KEY, remainingTime);
+        outState.putInt(REMAING_TAP_KEY, tapRemaming);
+
+        countDownTimer.cancel();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +53,55 @@ public class MainActivity extends AppCompatActivity {
         timerTextView.setText(remainingTime+"");
         thousandTextView.setText(tapRemaming+"");
 
-        countDownTimer = new CountDownTimer(initialCundDownInMillis, timeIntervals) {
+        if(savedInstanceState != null) {
+            remainingTime = savedInstanceState.getInt(REMANING_TIME_KEY);
+            tapRemaming = savedInstanceState.getInt(REMAING_TAP_KEY);
+
+            restoreGame();
+        }
+
+        if(savedInstanceState == null) {
+            countDownTimer = new CountDownTimer(initialCundDownInMillis, timeIntervals) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    remainingTime = (int) millisUntilFinished / 1000;
+                    timerTextView.setText(remainingTime + "");
+                }
+
+                @Override
+                public void onFinish() {
+                    Toast.makeText(MainActivity.this, "Countdown finished", Toast.LENGTH_SHORT).show();
+                    tapButton.setEnabled(false);
+                    if (tapRemaming > 0 && remainingTime == 0) {
+                        Toast.makeText(MainActivity.this, "Game Over", Toast.LENGTH_SHORT).show();
+                        showAlert("Game Over", "Would you like to reset the game?");
+                        isFirst = true;
+                    }
+                }
+            };
+        }
+    }
+
+    private void restoreGame() {
+        int resortedRemainingTime  = remainingTime;
+        int resortedTapsRemaining = tapRemaming;
+
+        thousandTextView.setText(resortedTapsRemaining + "");
+        timerTextView.setText(resortedRemainingTime+"");
+
+        countDownTimer = new CountDownTimer((long)resortedRemainingTime*1000 , timeIntervals) {
             @Override
             public void onTick(long millisUntilFinished) {
                 remainingTime = (int)millisUntilFinished/1000;
-                timerTextView.setText(remainingTime+ "");
+                timerTextView.setText(remainingTime +"");
             }
 
             @Override
             public void onFinish() {
                 Toast.makeText(MainActivity.this, "Countdown finished", Toast.LENGTH_SHORT).show();
-                tapButton.setEnabled(false);
-                if(tapRemaming > 0 && remainingTime == 0 ){
-                    Toast.makeText(MainActivity.this, "Game Over", Toast.LENGTH_SHORT).show();
-                    showAlert("Game Over", "Would you like to reset the game?");
-                    isFirst = true;
-                }
             }
         };
+        countDownTimer.start();
     }
 
     @Override
@@ -104,5 +151,19 @@ public class MainActivity extends AppCompatActivity {
 
         alertDialog.setCancelable(false);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_item){
+            Toast.makeText(MainActivity.this, BuildConfig.VERSION_NAME, Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
